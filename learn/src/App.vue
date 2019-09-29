b954143dead24862cc75cbf7ec6cebd33d48e700
liuhaokun <2568601148@qq.com>
2019-09-29 08:50:25 +0800

    提交

---
+++ learn/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+      <h2>提交Git</h2>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  box-sizing: border-box;
+
+}
+#app,html,body {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
